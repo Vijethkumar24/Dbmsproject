01e9f11252a7a9f41590810bfa28478377f2829c
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2023-01-03 20:08:24 +0530

    Add files via upload

---
+++ webhome.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Document</title>
+	<link href="style.css" rel="stylesheet" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link rel="stylesheet" type="text/css"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+	<link href="shortcut icon" href="https://fonts.google.com/icons?selected=Material%20Icons%3Aperson%3A" />
+</head>
+
+<body>
+	<header>
+		<img class="logo" src="img/tips/logo.png" alt="logo" />
+		<nav>
+			<div class="openMenu"><i class="fa fa-bars"></i></div>
+			<ul class="mainMenu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="profile.php">About us</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="signup.php">Login</a>
+				</li>
+				<div class="closeMenu"><i class="fa fa-times"></i></div>
+			</ul>
+		</nav>
+		<script src="main.js"></script>
+	</header>
+	<div class="main">
+		<h1 class="main-h1">BOOK YOUR'S</h1>
+		<h4 class="main-h4">GAS NOW!</h4>
+		<button class="shoping"><a href="login.php">BOOK</a></button>
+	</div>
+	<section class="testimonials">
+		<h2 class="text-center">Safety Tips For LPG Home - Do’s & Dont’s</h2>
+		<div class="container ">
+			<div class="row" id='box'>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12" style="text-align:center" x>
+							<u>
+								<h3><span class="glyphicon glyphicon-ok text-success"></span> DO'S</h3>
+							</u>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-success" style="padding:10px; width:60px;height:60px;"
+								src="img/tips/a1.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Use LPG in Ventilated Area</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-success" style="padding:10px; width:60px;height:60px"
+								src="img/tips/a2.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Before connecting, check for LPG leakage</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-success" style="padding:10px; width:60px;height:60px"
+								src="img/tips/a3.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Close the regulator knob once you finish cooking</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-success" style="padding:10px; width:60px;height:60px"
+								src="img/tips/a4.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Always keep the cylinder vertical, upright during usage and storage</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-success" style="padding:10px; width:60px;height:60px"
+								src="img/tips/a5.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Always install LPG pipeline above ground level</h3>
+						</div>
+
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-12" style="text-align:center">
+							<u>
+								<h3><span class="glyphicon glyphicon-remove text-danger"></span> DON'T</h3>
+							</u>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-danger" style="padding:10px; width:60px;height:60px" src="img/tips/b1.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Don’t place cylinders in closed areas.</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-danger" style="padding:10px; width:60px;height:60px" src="img/tips/b2.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Do not use any flammable objects near or over the burner</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-danger" style="padding:10px; width:60px;height:60px" src="img/tips/b3.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Do not switch on or off any lights or electrical equipment, smoke or use naked flames
+								incase of leakage</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-danger" style="padding:10px; width:60px;height:60px" src="img/tips/b4.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Do not roll the cylinder</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<img class="bg-danger" style="padding:10px; width:60px;height:60px" src="img/tips/b5.png" />
+						</div>
+						<div class="col-md-9">
+							<h4>Never store LPG in basement, cellar, elevated platforms</h3>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+		<footer class="footer">
+			<div class="container">
+				<div class="row1">
+					<div class="footer-col">
+						<h4>follow us</h4>
+						<div class="social-links">
+							<a href="#"><i class="fab fa-facebook-f"></i></a>
+							<a href="#"><i class="fab fa-twitter"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+							<a href="#"><i class="fab fa-linkedin-in"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+</body>
+
+</html>
